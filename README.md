4c22c315849c0d34c0f723ed1b79911a747886bc
welson ribeiro <welson.ribeirro@outlook.com>
2021-03-12 16:48:43 -0300

    :package: form orcamento

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Portifolio 2021
 ## Tarefas
 o controle das tarefas desse 
+
+## icones
+
+:package: nova funcionalidade
+:up: atualização
+:beatle: correção
+:checkered_flag: release
+
---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+var hamburguer = document.querySelector(".hamburguer");
+hamburguer.addEventListener("click", funcition(),{ 
+   Document:querySelector(".container").classList.toggle("show-menu")
+});
